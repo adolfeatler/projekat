c7a1fd3ca3cf1cfa5628f99599becee9750e4601
Pavle Djekic <pavle.g.djekic@gmail.com>
2023-08-30 16:38:52 +0200

    save

--- main.py
+++ main.py
@@ -31,6 +31,47 @@ class hyperlinkwindow(QDialog):
         self.textedit.insertHtml(hyperlink) 
         print ( self.textedit.toHtml())        
         self.close()
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "PyQt5 Open File"
+        self.top = 200
+        self.left = 500
+        self.width = 400
+        self.height = 300
+
+        self.InitWindow()
+
+    def InitWindow(self):
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        vbox = QVBoxLayout()
+
+        self.btn1 = QPushButton("Open Image")
+        self.btn1.clicked.connect(self.getImage)
+
+        vbox.addWidget(self.btn1)
+
+        self.label = QLabel("Hello")
+        vbox.addWidget(self.label)
+
+        self.setLayout(vbox)
+
+
+    def getImage(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file')
+
+        imagePath = fname[0]
+        pixmap = QPixmap(imagePath)
+        self.label.setPixmap(QPixmap(pixmap))
+        self.resize(pixmap.width(), pixmap.height())
+
+
+
 #pozivanje 
 class RTE(QMainWindow):
     def __init__(self):
@@ -117,6 +158,10 @@ class RTE(QMainWindow):
         colorbtn = QAction(QIcon("paint.png"), "colorset", self)
         colorbtn.triggered.connect(self.setFontColor)
         toolbar.addAction(colorbtn)
+
+        linkbtn = QAction(QIcon("link.png"), "linkset", self)
+        linkbtn.triggered.connect(self.open_hyperlink_window)
+        toolbar.addAction(linkbtn)
         
 
         self.addToolBar(toolbar)
@@ -157,7 +202,7 @@ class RTE(QMainWindow):
 
     def boldText(self):
         if  self.editor.fontWeight != QFont.Bold:
-              self.editdgdgdgor.setFontWeight(QFont.Bold)
+              self.editor.setFontWeight(QFont.Bold)
               return
         self.editor.setFontWeight(QFont.Normal)
 
@@ -170,7 +215,7 @@ class RTE(QMainWindow):
             with open(self.path, "w") as f:
                 f.write(text)
                 self.update_title()
-        except Expection as e:
+        except Exception as e:
             print(e)
 
     def file_saveas(self):
@@ -181,21 +226,10 @@ class RTE(QMainWindow):
         try:
             with open(path, 'w') as f:
                 f.write(text)
-                self.update_title()
+                self.update_title() 
         except Exception as e:
             print(e)
             
-    def picture_insert(self):
-        
-        self.acceptDrops()
-        self.label = QLabel(self)
-        self.pixmap = QPixmap("paste.png")
-        self.label.setPixmap(self.pixmap)
-        self.label.resize(self.pixmap.width(),
-                          self.pixmap.height())
-        self.show()
-        
-
 app = QApplication(sys.argv)
 window = RTE()
 window.show()
